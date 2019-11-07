1237e2eb495e257a736cb668ab771de4b945ac87
zhujiang <zhujiang@gmail.com>
2019-11-07 10:27:45 +0100

    add tiff2serial.py

--- piecewise/piecewise.py
+++ piecewise/piecewise.py
@@ -36,10 +36,10 @@ class D2Object:
         self.x[i - 1, j -1] = v
 
 class PieceWiseLinearRegression:
-    def __init__(self, t, y, potentialInflection, 
-                 debugLevel=1, 
-                 minSegmentCount=1, 
-                 maxSegmentCount=5, 
+    def __init__(self, t, y, potentialInflection,
+                 debugLevel=1,
+                 minSegmentCount=1,
+                 maxSegmentCount=5,
                  maxFirstInflection = 151,
                  minLastInflection = 273):
         self.y = y
@@ -50,23 +50,23 @@ class PieceWiseLinearRegression:
         self.maxSegmentCount = maxSegmentCount
         self.maxFirstInflection = maxFirstInflection
         self.minLastInflection = minLastInflection
-        
+
 
     def findSections(self):
-        epsilon = CEOF_DIFF_EPSILON 
+        epsilon = CEOF_DIFF_EPSILON
         threshold = CEOF_THRESHOLD
         # N = len(self.t)
-        
+
         M = (len(self.pc) + 1)
         r = [0] * M  # for storing max coef at M
         c = [[]]* M  # for storing inflection points at M
         ts = [0] * M # for storing time comsming at M
         sc = M # for storing section count
-        
+
 
         r[0] = - math.inf
         t1 = datetime.now()
-        
+
         for M in range(self.minSegmentCount, self.maxSegmentCount + 1): # M segments, c[1, ..., M-1]
             # print("current M segments: {0}".format(M))
             r[M], c[M] = self.MaxCorrcoef(M)
@@ -78,23 +78,23 @@ class PieceWiseLinearRegression:
                 print("-----------------------------------------")
                 print("for {0} pieces, time comsuming {1} secends:".format(M, ts[M] ))
                 print("max corrcoef {0} and breaks at {1}:".format(r[M], c[M]))
-            
+
             #stop iterator by following condition
             if r[M] > threshold:  # when max corrcoef is close to 1 (over threshold)
                 if self._debug > 0:
                     print("-----------------------------------------")
                     print("{0} piecewise  and split points:{1} with max corrcoef {2}. \n".format(M, c[M], r[M]))
                 #return r[M], c[M]
-                break;
-            if (r[M] - r[M -1]) < epsilon:# when corrcoef varies small enough during the iterate 
+                break
+            if (r[M] - r[M -1]) < epsilon:# when corrcoef varies small enough during the iterate
                 if self._debug > 0:
                     print("-----------------------------------------")
                     print("{0} piecewise  and split points:{1} with max corrcoef {2}. \n".format(M-1, c[M-1], r[M-1]))
                 #return r[M-1], c[M-1]
                 sc = M - 1
-                break;
+                break
         return r[sc], c[sc]
-        
+
 
     def MaxCorrcoef(self, M):
         if M == 1:
@@ -104,13 +104,13 @@ class PieceWiseLinearRegression:
         cs = combinations(self.pc, M - 1)
 #        if self._debug:
 #            print(cs)
-            
+
         max_c = None
         max_coef = -1
         for c in cs:
             if M>=4 and (c[0] > self.maxFirstInflection or c[-1] < self.minLastInflection):
                 continue
-            
+
             coef = self.calculateCorrceof(c)
             if coef > max_coef:
                 max_coef = coef
@@ -152,7 +152,7 @@ class PieceWiseLinearRegression:
 
 #            X = df[x_names]
 #            Y = df[y_names]
-            
+
             # print(df)
 
         # print(X, Y)
@@ -199,38 +199,38 @@ class Output:
     def flush(self):
         self.fo.close()
 
-        
-        
-def doMovingAverages(df, rolling_win_size=31):    
+
+
+def doMovingAverages(df, rolling_win_size=31):
     df['NDVI_5AVG'] = df['NDVI_2004'].rolling(window=5, center=True).mean()
     df['NDVI_11AVG'] = df['NDVI_2004'].rolling(window=11, center=True).mean()
     df['NDVI_AVG'] = df['NDVI_2004'].rolling(window=rolling_win_size, center=True).mean()
-    
+
     ndvi_avg = df['NDVI_AVG'].to_numpy()
     ndvi_avg_grd1 = np.abs(np.gradient(ndvi_avg)) * 10
     #ndvi_avg_grd2 = np.gradient(ndvi_avg,2) * 40
     ndvi_avg_grd2 = np.abs(np.gradient(np.gradient(ndvi_avg))) * 1000
-    
+
     df['NDVI_GRD1'] = ndvi_avg_grd1
     df['NDVI_GRD2'] = ndvi_avg_grd2
-  
-    
+
+
 def doPieceWise(df, numPotentialInflection=30):
     nlargest_row = df.nlargest(numPotentialInflection,'NDVI_GRD2')
     df['NLG_GRD'] = nlargest_row['NDVI_AVG']
     nlargest_day = np.sort(nlargest_row['DOY'].to_numpy())
-    
+
     if DEBUG_ENABLE:
         print("-----------------------------------------")
         print("potential inflection points",nlargest_day)
-    
+
     # print(df['NLG_GRD'])
-    
+
     t = df['DOY'].to_numpy()
     y = df['NDVI_2004'].to_numpy()
-    
+
     # print(t, y, nlargest_day)
- 
+
     # setup debug=True in PieceWise constructor, if want to print debug info
     print(nlargest_day)
     pwlr = PieceWiseLinearRegression(t, y, nlargest_day, debugLevel=0)
@@ -241,21 +241,21 @@ def doPieceWise(df, numPotentialInflection=30):
 def listPieceWiseByInflection(df, rolling_win_size, minNumPotentialInflection=5,  maxNumPotentialInflection=50, output=None):
     t1 = datetime.now()
     step = 2
-    
+
     for numInf in range(minNumPotentialInflection, maxNumPotentialInflection+1, step):
         max_coef, cp = doPieceWise(df, numInf)
         if not output is None:
             output.print("{0}\t{1}\t{2}\t{3}\t{4}".format(rolling_win_size, numInf, len(cp) + 1, max_coef, cp))
-    
+
     t2 = datetime.now()
     print("time comsuming for loop ({0},{1}:{2}): {3} seconds".format(minNumPotentialInflection, maxNumPotentialInflection, step, (t2-t1).seconds))
-        
+
 def doSeperateLinearRegression(df, cp):
     (cn_row, cn_col) = df.shape
     t = df['DOY'].to_numpy()
     y = df['NDVI_2004'].to_numpy()
     c = cp + (cn_row,)
-    
+
     start = 0
     preds = np.empty((1, 0))
     for end in c:
@@ -263,10 +263,10 @@ def doSeperateLinearRegression(df, cp):
         # Y = y[start: end].reshape(-1, 1)
         Y = y[start: end]
         # print(X, Y)
-    
+
         lm = linear_model.LinearRegression()
         lm.fit(X, Y)
-        
+
         # predictions = lm.predict(x_data)
         #score = lm.score(X,Y)
         lm.score(X,Y)
@@ -281,51 +281,51 @@ def doSeperateLinearRegression(df, cp):
         preds = np.append(preds, predictions)
         start= end
         # start= end -1  # sorry to disconnect inflaction because of preds = np.append(preds, predictions)
-        
+
     #preds_df = pd.DataFrame(preds,columns=['X', 'Y', 'NDVI_PRED'])
     # print(preds, preds.size)
     df['NDVI_PRED'] = preds
-    
-    
+
+
 def doDisplay(df):
     # Plot outputs
     (cn_row, cn_col) = df.shape
     x = df['DOY']
     ndvi =df['NDVI_2004']
     nlg =df['NLG_GRD']
-    
+
     ndvi_avg =df['NDVI_AVG']
     ndvi_avg_5 =df['NDVI_5AVG']
     ndvi_avg_11 =df['NDVI_11AVG']
     nlg =df['NLG_GRD']
-    
+
     ndvi_grd1 = df['NDVI_GRD1']
     ndvi_grd2 = df['NDVI_GRD2']
-    
+
     ndvi_pred = df['NDVI_PRED']
-    
-    
+
+
     plt.figure(figsize=FIG_SIZE)
-    plt.xlim(left=0, right=cn_row) 
-    plt.ylim(bottom=0, top=0.4) 
-    
+    plt.xlim(left=0, right=cn_row)
+    plt.ylim(bottom=0, top=0.4)
+
     plt.scatter(x, nlg,  color='black')
     annos = df[pd.notna(df['NLG_GRD'])][['DOY', 'NLG_GRD']]
     # print(annos)
-    
+
     old_x = old_y = 1e9 # make an impossibly large initial offset
     thresh = 2 #make a distance threshold
     for row in annos.itertuples():
         px,py = row[1], row[2]
         label = "{}".format(px)
-        
+
         #calculate distance
         d = ((px-old_x)**2+(py-old_y)**2)**(.5)
         #if distance less than thresh then flip the arrow
         flip = 1
         # print(d)
         if d < thresh: flip=-2
-    
+
         plt.annotate(
             label,
             xy = (px, py), xytext = (-20*flip, 5*flip),
@@ -343,7 +343,7 @@ def doDisplay(df):
     ndvi_grd1_line, = plt.plot(x, ndvi_grd1, label="NDVI_GRD1", linewidth=1)
     ndvi_grd2_line, = plt.plot(x, ndvi_grd2, label="NDVI_GRD2", linewidth=1)
     ndvi_pred_line, = plt.plot(x, ndvi_pred,color='blue', label="NDVI_PRED", linewidth=1)
-    
+
     handles=[ndvi_line,
              ndvi_avg_line,
              ndvi_avg_line5,
@@ -351,14 +351,14 @@ def doDisplay(df):
              ndvi_grd1_line,
              ndvi_grd2_line,
              ndvi_pred_line]
-    
+
     plt.legend(handles = handles, loc='upper right')
 
-    
+
     #df.plot(x='DOY', y=['NDVI_2004','NDVI_AVG','NDVI_5AVG', 'NDVI_11AVG','NDVI_GRD1', 'NDVI_GRD2','NLG_GRD', 'NDVI_PRED'],figsize= FIG_SIZE)
     #df.plot.scatter(x='DOY', y='NLG_GRD',figsize= FIG_SIZE)
-    
-    
+
+
 # paramters that affact the result
 #
 # the sie of figure, unit: inch
@@ -375,7 +375,7 @@ DEBUG_ENABLE = False
 
 
 # ----------------------------
-# explore ndvi data and find potential segment points 
+# explore ndvi data and find potential segment points
 df = pd.read_csv("/Users/jiangzhu/workspace/igsnrr/data/ndvi/ndvi_2004.txt", sep='\t')
 #df = pd.read_csv("/Users/jiangzhu/workspace/igsnrr/data/ndvi/ndvi_2004.txt", sep='\t', index_col=1)
 
---
+++ piecewise/readme.txt
@@ -0,0 +1,14 @@
+1. install pyenv
+$ pip install pipenv
+
+2. active virtual python env
+$ pyenv install 3.7.4
+$ pyenv shell 3.7.4
+
+3. install 3rd party package
+$ pip install numpy scipy matplotlib ipython jupyter pandas sympy nose
+-- for mac
+$ pip install rasterio
+-- for windows
+$ pip install GDAL-1.11.2-cp27-none-win32.whl rasterio-0.24.0-cp27-none-win32.whl
+(ref: https://rasterio.readthedocs.io/en/stable/installation.html)
