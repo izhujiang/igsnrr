5c3903644636d5f5cf68e1f8250333a32d3d3f08
jiang zhu <m.zhujiang@gmail.com>
2020-08-06 23:22:25 +0200

    fix: mod13q1-geotiff-join.py support multiple bands

---
+++ MOD13Q1/join.sh
@@ -0,0 +1,1 @@
+python ./mod13q1-geotiff-join.py /Users/jiangzhu/workspace/igsnrr/data/MOD13Q1/MOD13Q1-MEDIAN-XJ /Users/jiangzhu/workspace/igsnrr/data/MOD13Q1/MOD13Q1-MEDIAN-XJ-join MOD13Q1-MEDIAN-XJ
--- MOD13Q1/mod13q1-geotiff-join.py
+++ MOD13Q1/mod13q1-geotiff-join.py
@@ -1,35 +1,69 @@
-
 # -*- coding: utf-8 -*-
 import os
+import argparse
 import rasterio
 
-dataRoot = '/Users/jiangzhu/workspace/igsnrr/data/MOD13Q1'
-inputDir = os.path.join(dataRoot, 'MOD13Q1-MEDIAN-XJ')
-outputDir = os.path.join(dataRoot, 'MOD13Q1-MEDIAN-XJ-join')
-if not os.path.exists(outputDir):
-    os.makedirs(outputDir)
 
-files = sorted(os.listdir(inputDir))
+def joinFiles(inputdir, outputDir, fprefix):
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
 
-fInput = os.path.join(inputDir, files[0])
-profile = None
-with rasterio.open(fInput) as dt_in:
-    profile = dt_in.profile.copy()
-    profile.update({
-            'count': len(files),
-            'dtype': 'float32',
-            'height': dt_in.height,
-            'width': dt_in.width
-            })
-fOutput = os.path.join(outputDir, 'MOD13Q1-MEDIAN-XJ-join.tif')
+    files = sorted(os.listdir(inputDir))
 
-with rasterio.open(fOutput, 'w', **profile) as dt_out:
-    year_index = 1
+    fInput = os.path.join(inputDir, files[0])
+    profile = None
+    descriptions = None
+    with rasterio.open(fInput) as dt_in:
+        profile = dt_in.profile.copy()
+        profile.update({
+                'count': len(files),
+                'dtype': 'float32',
+                'height': dt_in.height,
+                'width': dt_in.width
+                })
+        # print("profile", profile)
+        # print("descriptions", dt_in.descriptions)
+        descriptions = dt_in.descriptions
 
-    for fInput in files:
-        fInput = os.path.join(inputDir, fInput)
-        print(fInput)
-        with rasterio.open(fInput) as dt_in:
-            ndvi_arr = dt_in.read(1)
-            dt_out.write_band(year_index, ndvi_arr)
-            year_index += 1
+    for bandId, bandname in enumerate(descriptions):
+        print("\njoin band {0} {1} ...".format(bandId+1, bandname))
+        fName = outputFilePrefix + "-" + bandname + ".tif"
+        fOutput = os.path.join(outputDir, fName)
+        print("write to {0} ...".format(fOutput))
+
+        with rasterio.open(fOutput, 'w', **profile) as dt_out:
+            year_index = 1
+            for fo in files:
+                fInput = os.path.join(inputDir, fo)
+                print(fInput)
+                with rasterio.open(fInput) as dt_in:
+                    ndvi_arr = dt_in.read(bandId + 1)
+                    dt_out.write_band(year_index, ndvi_arr)
+                    year_index += 1
+
+
+if __name__ == "__main__":
+    workspace = "/Users/jiangzhu/workspace/igsnrr/data/MOD13Q1/"
+    inputDir = os.path.join(workspace, "MOD13Q1-MEDIAN-XJ")
+    outputDir = os.path.join(workspace, "MOD13Q1-MEDIAN-XJ-join")
+    outputFilePrefix = "MOD13Q1-MEDIAN-XJ-join"
+
+    parser = argparse.ArgumentParser(
+            description="Join multiple tiff into serial files")
+    parser.add_argument(
+            "srcDir",
+            help="input directory for geotiff files ")
+    parser.add_argument(
+            "destDir",
+            help="output directory to store serial files")
+    parser.add_argument(
+            "prefix",
+            help="prefix for serial files")
+
+    args = parser.parse_args()
+    print("input arguments: ", args)
+
+    inputDir = args.srcDir
+    outputDir = args.destDir
+    prefix = args.prefix
+    joinFiles(inputDir, outputDir, prefix)
