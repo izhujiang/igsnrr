a94a0e6037c85d790a4ca1462cd7a139248e8a58
jiang zhu <m.zhujiang@gmail.com>
2021-05-22 00:28:48 +0200

    feat: add minSegmentLength parameter to pwlfm.py

--- piecewise/pw-demo.py
+++ piecewise/pw-demo.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+# from datetime import datetime
 
 import numpy as np
 import pandas as pd
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 
 
 from enum import Enum
+
+
 class ModelType(Enum):
     JEKEL_PWLF = "jekel_pwlf_PiecewiseLinFit"
     IGSNRR_PWLFM = "igsnrr_pwlfm_PieceWiseLinearFitModel"
@@ -18,6 +20,7 @@ class PieceWiseLinearRegressionContext:
             recordNumber,
             minSegmentCount=1,
             maxSegmentCount=4,
+            minSegmentLength=1,
             ceofDiffEpsilon=0.00001,
             ceofThreshold=0.99,
             debugLevel=0, **kwargs):
@@ -26,6 +29,7 @@ class PieceWiseLinearRegressionContext:
 
         self.minSegmentCount = minSegmentCount
         self.maxSegmentCount = maxSegmentCount
+        self.minSegmentLength = minSegmentLength
         self.threshold = ceofThreshold
         self.epsilon = ceofDiffEpsilon
 
@@ -43,16 +47,17 @@ class PieceWiseLinearRegressionContext:
 
         self.debug = debugLevel
 
+
 # https://github.com/cjekel/piecewise_linear_fit_py
 class PieceWiseLinearResult:
     def __init__(
             self,
-            corcoef, 
-            regcoefs, 
-            breaks, 
+            corcoef,
+            regcoefs,
+            breaks,
             generalCorcoef,
-            generalPvalue, 
-            psCorcoefs, 
+            generalPvalue,
+            psCorcoefs,
             psPvalues,
             yPred):
         self.corcoef = corcoef
@@ -63,7 +68,7 @@ class PieceWiseLinearResult:
         self.psCorcoefs = psCorcoefs
         self.psPvalues = psPvalues
         self.yPred = yPred
-   
+
 
 def doPieceWise(model, x, y, ctx):
     minSeg = ctx.minSegmentCount
@@ -72,18 +77,18 @@ def doPieceWise(model, x, y, ctx):
     crs = [0] * (maxSeg + 1)  # for storing max correlation coefficient at M
     pvs = [0] * (maxSeg + 1)   # for storing p-value for coefficient at M
     rrs = [[]] * (maxSeg + 1)  # for storing max regression coefficient at M
-    yps = [[]] * (maxSeg + 1)  # for storing predictions with max correlation coefficient at M
+    # for storing predictions with max correlation coefficient at M
+    yps = [[]] * (maxSeg + 1)
     ips = [[]] * (maxSeg + 1)  # for storing inflection points at M
-    
+
     for M in range(minSeg, maxSeg+1):
-        # fit the data for M line segments
-        
-        model.fit(M)
+        print("fit the data for {0} line segments".format(M))
+
+        model.fit(M, minSegmentLength=ctx.minSegmentLength)
         # fitfast(self, n_segments, pop=2, bounds=None, \*\*kwargs)
         rrs[M] = model.beta
         ips[M] = model.fit_breaks.round(decimals=1)
 
-        
         # predict for the determined points
         yPred = model.predict(x)
         yps[M] = yPred
@@ -99,34 +104,36 @@ def doPieceWise(model, x, y, ctx):
         if abs(crs[M] - crs[M - 1]) < ctx.epsilon:
             sc = M - 1
             break
-    
+
     # ip = ips[sc].round(decimals=1)
     ip = ips[sc]
-    r_pw, p_values_pw  = calculateCeofsByPiece(x, y, yps[sc], ip)
-    pwRes = PieceWiseLinearResult(crs[sc], 
-        rrs[sc], 
-        ips[sc], 
-        crs[sc], 
-        pvs[sc], 
-        r_pw, 
-        p_values_pw,
-        yps[sc])
+    r_pw, p_values_pw = calculateCeofsByPiece(x, y, yps[sc], ip)
+    pwRes = PieceWiseLinearResult(
+            crs[sc],
+            rrs[sc],
+            ips[sc],
+            crs[sc],
+            pvs[sc],
+            r_pw,
+            p_values_pw,
+            yps[sc])
 
     return pwRes
 
- # calclate Corrceof piece by piece
- # cc: [x[0], c1, c2, ... x[-1]]
+
+# calclate Corrceof piece by piece
+# cc: [x[0], c1, c2, ... x[-1]]
 def calculateCeofsByPiece(x, y, yp, cc):
     M = len(cc) - 1
 
     rs = [0] * M
     p_values = [0] * M
-   
+
     p0 = x[0]
     for i in range(0, M):
         p1 = cc[i+1]
-        indexs = np.logical_and(x >= p0 , x <= p1)
-        if len(x[indexs]) < 2: 
+        indexs = np.logical_and(x >= p0, x <= p1)
+        if len(x[indexs]) < 2:
             print(cc)
             print("skipping p0-p1:", p0, p1)
             continue
@@ -150,9 +157,11 @@ def exportResult(filepath, pwResArr, df):
         msgs.append(msg)
         msg = "reg_coef: {0}\n".format(psRes.regcoefs)
         msgs.append(msg)
-        msg = "general correlation coefficient: {0}\n".format(psRes.generalCorcoef)
+        msg = "general correlation coefficient: {0}\n".format(
+                psRes.generalCorcoef)
         msgs.append(msg)
-        msg = "general correlation p_values : {0}\n".format(psRes.generalPvalue)
+        msg = "general correlation p_values : {0}\n".format(
+                psRes.generalPvalue)
         msgs.append(msg)
         msg = "cor_coef_piecewise: {0}\n".format(psRes.psCorcoefs)
         msgs.append(msg)
@@ -165,20 +174,21 @@ def exportResult(filepath, pwResArr, df):
         fo.writelines(msgs)
 
     filepath = filepath.replace(".", ".det.")
-    
+
     df.to_csv(filepath, sep="\t", index=False, float_format='%10.6f')
+
 
 def exportImage(model, x, y, breaks, imgPath, showImg=False):
     model.fit_with_breaks(breaks)
     xHat = np.linspace(min(x), max(x), num=1000)
     yHat = model.predict(xHat)
-    
+
     plt.figure()
     plt.plot(x, y, 'o')
     plt.plot(xHat, yHat, '-')
-    
+
     plt.savefig(imgPath, dpi=150)
-    if showImg == True:
+    if showImg:
         plt.show()
 
 
@@ -186,7 +196,7 @@ if __name__ == "__main__":
     # using_model = ModelType.JEKEL_PWLF
     # or
     using_model = ModelType.IGSNRR_PWLFM
-    
+
     if using_model == ModelType.JEKEL_PWLF:
         import pwlf
     else:
@@ -194,21 +204,22 @@ if __name__ == "__main__":
 
     input_filepath = "/Users/hurricane/share/data.txt"
     df = pd.read_csv(input_filepath, sep='\t')
-    
+
     nrow, ncol = df.shape
     da = df.to_numpy()
     ctx = PieceWiseLinearRegressionContext(
         recordNumber=nrow,
         minSegmentCount=1,
         maxSegmentCount=4,
+        minSegmentLength=3,
         ceofThreshold=0.99,
         ceofDiffEpsilon=0.0000001,
         debugLevel=0,
     )
-    
+
     x = da[:, 0]
     xName = df.columns[0]
-    dfRes = pd.DataFrame(data = {
+    dfRes = pd.DataFrame(data={
         xName: x})
     pwResArr = [None] * (ncol-1)
     for j in range(1, ncol):
@@ -220,16 +231,16 @@ if __name__ == "__main__":
         if using_model == ModelType.JEKEL_PWLF:
             model = pwlf.PiecewiseLinFit(x, y)
         else:
-            model = pwlfm.PieceWiseLinearFitModel(x,y)
-        
+            model = pwlfm.PieceWiseLinearFitModel(x, y)
+
         pwRes = doPieceWise(model, x, y, ctx)
-        pwResArr[j-1] = {"name": yName, "value":pwRes}
-        dfRes[yName] =  y
+        pwResArr[j-1] = {"name": yName, "value": pwRes}
+        dfRes[yName] = y
         dfRes[yNamePred] = pwRes.yPred
 
         imgPath = input_filepath.replace(".txt", "_res_{0}.png".format(yName))
         exportImage(model, x, y, pwRes.breaks, imgPath)
-    
+
     reuslt_filepath = input_filepath.replace(".", "_res.")
     dfRes = dfRes.convert_dtypes()
     exportResult(reuslt_filepath, pwResArr, dfRes)
--- piecewise/pwlfm.py
+++ piecewise/pwlfm.py
@@ -1,4 +1,4 @@
-import math
+# import math
 from itertools import combinations
 import numpy as np
 
@@ -17,64 +17,71 @@ class PieceWiseLinearFitModel:
         self.y = y
         self.t = x
         self.lm = linear_model.LinearRegression()
-        
 
-    def fit(self, numSegCount, potentialBreaks=None, maxFirstBreak=None, minLastBreak=None):
+    def fit(self, n_segments, minSegmentLength=1,
+            potentialBreaks=None, maxFirstBreak=None, minLastBreak=None):
         """
         model.ssr # sum-of-squares error
-        model.fit_breaks # breakpoint locations 
-        model.n_parameters # number of model parameters 
-        model.n_segments # number of line segments 
+        model.fit_breaks # breakpoint locations
+        model.n_parameters # number of model parameters
+        model.n_segments # number of line segments
         model.beta # model parameters
-        model.slopes # slope of each line segment 
+        model.slopes # slope of each line segment
         model.intercepts # y intercepts of each line segment
         """
 
         max_c = None
         max_cor_coef = 0
-        
-        if potentialBreaks == None:
+        numSegCount = n_segments
+
+        if potentialBreaks is None:
             potentialBreaks = self.t[1:-1]
 
         if maxFirstBreak is None:
             maxFirstBreak = self.t[-2]
-        
+
         if minLastBreak is None:
             minLastBreak = self.t[1]
 
         if numSegCount <= 1:
             c = np.concatenate((self.t[:1], self.t[-1:]), axis=0)
             self.fit_with_breaks(c)
-            return 
+            return
 
         # print(potentialBreaks)
         cs = combinations(potentialBreaks, numSegCount - 1)
         # find the breaks with max cor_coef
         for c in cs:
-            if (c[0] > maxFirstBreak
-                or c[-1] < minLastBreak):
+            if (c[0] > maxFirstBreak or c[-1] < minLastBreak):
                 continue
             c = np.concatenate((self.t[:1], np.array(c), self.t[-1:]), axis=0)
+            if minSegmentLength > 1:
+                c0 = np.concatenate((c[:1], c[:-1]), axis=0)
+                c_diff = np.subtract(c, c0)
+                if np.amin(c_diff[1:]) < minSegmentLength:
+                    continue
+
+            print("fitting with:", c)
             self.fit_with_breaks(c)
-            
+
             yp = self.predict()
             cor_ceofs = np.corrcoef(yp, self.y)
-            
-            if abs(cor_ceofs[0,1]) > max_cor_coef:
-                max_cor_coef = abs(cor_ceofs[0,1])
+
+            if abs(cor_ceofs[0, 1]) > max_cor_coef:
+                max_cor_coef = abs(cor_ceofs[0, 1])
                 max_c = c
-        # fill self object 
+        # fill self object
         self.fit_with_breaks(max_c)
         return max_c
 
     def fit_with_breaks(self, breaks):
         """
         model.ssr # sum-of-squares error
-        model.fit_breaks # breakpoint locations 
-        model.n_parameters # number of model parameters 
-        model.n_segments # number of line segments 
+        model.fit_breaks # breakpoint locations
+        model.n_parameters # number of model parameters
+        model.n_segments # number of line segments
         model.beta # model parameters
-        model.slopes # slope of each line segment 
+        model.slopes # slope of each line segment
         model.intercepts # y intercepts of each line segment
         """
         # M = len(c) + 1
@@ -82,18 +89,17 @@ class PieceWiseLinearFitModel:
 
         self.X = self.setupVirtualX(self.t, breaks)
         self.Y = self.y.reshape((N, 1))
-        
+
         self.lm.fit(self.X, self.Y)
         self.beta = self.lm.coef_.flatten()
         self.fit_breaks = breaks
         self.n_parameters = len(self.beta)
         self.n_segments = self.n_parameters - 1
 
-        return 
+        return
 
-    
-    # T[0, 1, ..., N-1, N] 
-    # C[0, 1, ..., M-1, M] 
+    # T[0, 1, ..., N-1, N]
+    # C[0, 1, ..., M-1, M]
     def setupVirtualX(self, T, C):
         # N = len(t)
         M = len(C)
@@ -103,19 +109,21 @@ class PieceWiseLinearFitModel:
             t = vArr[0]
             x = np.zeros(M)
             x[0] = 1
-            for j in range(1, M): # test where is t located [ *, C[j-1], *, C[j], *]
+
+            for j in range(1, M):
+                # test where is t located [ *, C[j-1], *, C[j], *]
                 if t > C[j]:
                     x[j] = C[j] - C[j-1]
-                elif t < C[j -1]:
+                elif t < C[j - 1]:
                     break
                 else:  # C[j-1] <= t <= C[j]
                     x[j] = t - C[j-1]
             return x
-            
+
         X = np.apply_along_axis(vFunc, 1, TT)
         # print(C, X)
 
-        return X[:,1:]
+        return X[:, 1:]
 
     def predict(self, x=None):
         if x is None:
@@ -123,7 +131,7 @@ class PieceWiseLinearFitModel:
         else:
             X = self.setupVirtualX(x, self.fit_breaks)
             yp = self.lm.predict(X).flatten()
-        
+
         return yp
 
 
@@ -131,11 +139,10 @@ if __name__ == "__main__":
     input_filepath = "/Users/hurricane/share/data.txt"
     import pandas as pd
     df = pd.read_csv(input_filepath, sep='\t')
-    
+
     nrow, ncol = df.shape
     da = df.to_numpy()
-    
-    
+
     x = da[:, 0]
     y = da[:, 1]
     xName = df.columns[0]
@@ -144,12 +151,12 @@ if __name__ == "__main__":
 
     # initialize piecewise linear fit with your x and y data
     model = PieceWiseLinearFitModel(x, y)
-    model.fit(4)
+    model.fit(4, minSegmentLength=3)
     # fitfast(self, n_segments, pop=2, bounds=None, \*\*kwargs)
-    
+
     print(model.fit_breaks)
     print(model.beta)
-    
+
     # predict for the determined points
     yp = model.predict()
     print("y:", y)
