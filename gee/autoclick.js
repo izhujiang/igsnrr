ec1307fadefa290d10ef49f9cf129366bbd568d2
Jiang Zhu <m.zhujiang@gmail.com>
2023-03-23 13:15:20 +0100

    feat: support autoclick on gee

---
+++ gee/autoclick.js
@@ -0,0 +1,23 @@
+function runTaskList() {
+  var tasklist = document
+    .getElementById("task-pane")
+    .shadowRoot.querySelectorAll(
+      "div.task.legacy.type-EXPORT_IMAGE.awaiting-user-config"
+    );
+  for (var i = 0; i < tasklist.length; i++)
+    tasklist[i].getElementsByClassName("run-button")[0].click();
+}
+
+function confirmAll() {
+  var diags = document.getElementsByTagName("ee-image-config-dialog");
+  console.log(diags.length);
+  for (var i = 0; i < diags.length; i++) {
+    run =
+      diags[i].shadowRoot.children[0].shadowRoot.querySelector(".ok-button");
+    run.click();
+  }
+}
+runTaskList();
+setTimeout(() => {
+  confirmAll();
+}, 5000);
